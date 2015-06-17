d4e36b8c95a52defd07cab9ef51021853946879d
Francois Daoust <fd@tidoust.net>
2015-06-17 15:16:38 +0200

    Modularized video controlling logic into a Sequencer class
    The sequencer class should (in theory at least, not yet tested) be
    able to control multiple media elements at once, including audio, video
    and media controllers.

--- examples/browser/video.js
+++ examples/browser/video.js
@@ -10,12 +10,13 @@ require.config({
 require([
   'src/TimingObject',
   'src/SocketTimingProvider',
+  'src/Sequencer',
   'src/StateVector',
   'woodman',
   'examples/browser/woodmanConfig',
   'examples/browser/WoodmanElementAppender'
 ], function (
-    TimingObject, SocketTimingProvider, StateVector,
+    TimingObject, SocketTimingProvider, Sequencer, StateVector,
     woodman, woodmanConfig, ElementAppender) {
 
   /**********************************************************************
@@ -25,26 +26,6 @@ require([
   var roundFloat = function (nb) {
     return Math.round(nb * 1000) / 1000;
   };
-
-
-  /**********************************************************************
-  Global settings to tweak the behavior of the demo when synchronization
-  becomes fairly bad.
-  **********************************************************************/
-  // The video is considered in sync with the timing object if the difference
-  // between its reported position and the position of the timing object is
-  // below that threshold (in seconds).
-  var minDiff = 0.010;
-
-  // Maximum delay for catching up (in seconds).
-  // If the code cannot meet the maxPlaybackRateDelta and maxDelay constraints,
-  // it will have the video directly seek to the right position.
-  var maxDelay = 1.0;
-
-  // Amortization period (in seconds).
-  // The amortization period is used when adjustments are made to
-  // the playback rate of the video.
-  var amortPeriod = 2.0;
 
 
   /**********************************************************************
@@ -87,129 +68,56 @@ require([
 
 
   /**********************************************************************
-  Synchronize video with timing information
+  Control buttons do not directly control the video, they just send the
+  right update command to the underlying timing object. The video
+  effectively starts to play or stops playing when the timing object is
+  updated.
   **********************************************************************/
-  var timingVector = null;
-  var videoVector = null;
-  var videoDriftRate = 0.0;
-  var videoSeeked = false;
-  var amortTimeout = null;
+  buttons.play.addEventListener('click', function () {
+    logger.info('play video');
+    sequencer.play();
+  });
 
-  // Whenever the timing object issues a "change" event, we should ensure
-  // the video is aligned with it. If the video was in the middle of an
-  // amortization period, we need to cancel it first.
-  var onTimingChange = function () {
-    if (amortTimeout) {
-      // Cancel amortization period, no update to video drift and playback
-      // rate, that will be taken care of by the call to controlVideo
-      logger.info('cancel amortization period');
-      clearTimeout(amortTimeout);
-      amortTimeout = null;
-      videoSeeked = false;
-    }
-    controlVideo();
-  };
+  buttons.pause.addEventListener('click', function () {
+    logger.info('pause video');
+    sequencer.pause();
+  });
 
-
-  var controlVideo = function () {
-    var diff = 0.0;
-
-    // Do not adjust anything during the amortization period
-    if (amortTimeout) {
-      return;
-    }
-
-    // Get new readings from Timing object
-    timingVector = timing.query();
-
-    if ((timingVector.velocity === 0.0) &&
-        (timingVector.acceleration === 0.0)) {
-      logger.info('stop video and seek to right position');
-      video.pause();
-      video.currentTime = timingVector.position;
-      videoVector = new StateVector(timingVector);
-    }
-    else if (video.paused) {
-      logger.info('play video');
-      videoVector = new StateVector({
-        position: timingVector.position,
-        velocity: timingVector.velocity + videoDriftRate,
-        acceleration: 0.0,
-        timestamp: timingVector.timestamp
-      });
-      videoSeeked = true;
-      video.currentTime = videoVector.position;
-      video.playbackRate = videoVector.velocity;
-      video.play();
-      logger.info('start amortization period',
-          'diff=' + diff, 'playbackRate=' + videoVector.velocity);
-      amortTimeout = setTimeout(stopAmortizationPeriod, amortPeriod * 1000);
-    }
-    else {
-      videoVector = new StateVector({
-        position: video.currentTime,
-        velocity: videoVector.velocity,
-      });
-      diff = timingVector.position - videoVector.position;
-      if (Math.abs(diff) < minDiff) {
-        logger.info('video and vector are in sync!');
-      }
-      else if (Math.abs(diff) > maxDelay) {
-        logger.info('seek video and start amortization period',
-          'diff=' + diff, 'pos=' + timingVector.position);
-        videoVector.position = timingVector.position;
-        videoVector.velocity = timingVector.velocity + videoDriftRate;
-        video.currentTime = videoVector.position;
-        video.playbackRate = videoVector.velocity;
-        videoSeeked = true;
-        amortTimeout = setTimeout(stopAmortizationPeriod, amortPeriod * 1000);
-      }
-      else {
-        videoVector.velocity =
-          videoDriftRate +
-          (timingVector.computePosition(timingVector.timestamp + amortPeriod) -
-            videoVector.position) / amortPeriod;
-        video.playbackRate = videoVector.velocity;
-        logger.info('start amortization period',
-          'diff=' + diff, 'playbackRate=' + videoVector.velocity);
-        amortTimeout = setTimeout(stopAmortizationPeriod, amortPeriod * 1000);
-      }
-    }
-    renderStateInfo();
-  };
+  buttons.stop.addEventListener('click', function () {
+    logger.info('stop video');
+    sequencer.pause();
+    sequencer.currentTime = 0.0;
+  });
 
 
   /**********************************************************************
-  Stops the playback adjustment once the amortization period is over
+  Display timing object and video positions
   **********************************************************************/
-  var stopAmortizationPeriod = function () {
-    var now = Date.now() / 1000.0;
-
-    // Don't adjust playback rate and drift rate if video was seeked.
-    // The seek operation often introduces artificial delays.
-    if (videoSeeked) {
-      logger.info('end of amortization period for seek');
-      videoSeeked = false;
-      amortTimeout = null;
-      return;
+  var maxDiff = {
+    position: 0,
+    velocity: 0
+  };
+  var renderStateInfo = function () {
+    var position = sequencer.currentTime;
+    var velocity = sequencer.playbackRate;
+    var diff = {
+      position: roundFloat(position - video.currentTime),
+      velocity: roundFloat(velocity - video.playbackRate)
+    };
+    info.timing.position.innerHTML = roundFloat(position);
+    info.timing.velocity.innerHTML = roundFloat(velocity);
+    info.video.position.innerHTML = roundFloat(video.currentTime);
+    info.video.velocity.innerHTML = roundFloat(video.playbackRate);
+    info.diff.position.innerHTML = diff.position;
+    info.diff.velocity.innerHTML = diff.velocity;
+    if (Math.abs(diff.position) > Math.abs(maxDiff.position)) {
+      maxDiff.position = diff.position;
+      info.diff.max.position.innerHTML = diff.position;
     }
-
-    // Compute the difference between the position the video should be and
-    // the position it is reported to be at.
-    var diff = videoVector.computePosition(now) - video.currentTime;
-
-    // Compute the new video drift rate
-    videoDriftRate = diff / (now - videoVector.timestamp);
-
-    // Switch back to the current vector's velocity,
-    // adjusted with the newly computed drift rate
-    videoVector.velocity = timingVector.velocity + videoDriftRate;
-    video.playbackRate = videoVector.velocity;
-
-    logger.info('end of amortization period',
-      'new drift=' + videoDriftRate,
-      'playback rate=' + videoVector.velocity);
-    amortTimeout = null;
+    if (Math.abs(diff.velocity) > Math.abs(maxDiff.velocity)) {
+      maxDiff.velocity = diff.velocity;
+      info.diff.max.velocity.innerHTML = diff.velocity;
+    }
   };
 
 
@@ -223,75 +131,22 @@ require([
   timing.srcObject = timingProvider;
   logger.info('create timing object connected to socket... done');
 
+  logger.info('create sequencer...');
+  var sequencer = new Sequencer(timing);
+  sequencer.addMediaElement(video);
+  logger.info('create sequencer... done');
+
   logger.info('add listener to "timeupdate" events...');
-  timing.addEventListener('timeupdate', controlVideo);
-  timing.addEventListener('change', onTimingChange);
-  logger.info('add listener to "timeupdate" events... done');
+  sequencer.addEventListener('timeupdate', renderStateInfo);
 
   logger.info('add listener to "readystatechange" events...');
-  timing.addEventListener('readystatechange', function (evt) {
+  sequencer.addEventListener('readystatechange', function (evt) {
     logger.info('readystatechange event', 'state=' + evt.value);
     if (evt.value === 'open') {
       start();
     }
   });
   logger.info('add listener to "readystatechange" events... done');
-
-
-  /**********************************************************************
-  Control buttons do not directly control the video, they just send the
-  right update command to the underlying timing object. The video
-  effectively starts to play or stops playing when the timing object is
-  updated.
-  **********************************************************************/
-  buttons.play.addEventListener('click', function () {
-    logger.info('set timing object in motion...');
-    timing.update(null, 1.0);
-    logger.info('set timing object in motion... command sent');
-  });
-
-  buttons.pause.addEventListener('click', function () {
-    logger.info('pause motion...');
-    timing.update(null, 0.0);
-    logger.info('pause motion... command sent');
-  });
-
-  buttons.stop.addEventListener('click', function () {
-    logger.info('stop motion...');
-    timing.update(0.0, 0.0);
-    logger.info('stop motion... command sent');
-  });
-
-
-  /**********************************************************************
-  Display timing object and video positions
-  **********************************************************************/
-  var maxDiff = {
-    position: 0,
-    velocity: 0
-  };
-  var renderStateInfo = function () {
-    var vector = timing.query();
-    var diff = {
-      position: roundFloat(vector.position - video.currentTime),
-      velocity: roundFloat(vector.velocity - video.playbackRate)
-    };
-    info.timing.position.innerHTML = roundFloat(vector.position);
-    info.timing.velocity.innerHTML = roundFloat(vector.velocity);
-    info.video.position.innerHTML = roundFloat(video.currentTime);
-    info.video.velocity.innerHTML = roundFloat(video.playbackRate);
-    info.video.drift.innerHTML = roundFloat(videoDriftRate);
-    info.diff.position.innerHTML = diff.position;
-    info.diff.velocity.innerHTML = diff.velocity;
-    if (Math.abs(diff.position) > Math.abs(maxDiff.position)) {
-      maxDiff.position = diff.position;
-      info.diff.max.position.innerHTML = diff.position;
-    }
-    if (Math.abs(diff.velocity) > Math.abs(maxDiff.velocity)) {
-      maxDiff.velocity = diff.velocity;
-      info.diff.max.velocity.innerHTML = diff.velocity;
-    }
-  };
 
 
   /**********************************************************************
---
+++ src/Sequencer.js
@@ -0,0 +1,402 @@
+/**
+ * @file A sequencer takes inputs from a timing object and harnesses one or
+ * more HTML media elements (audio, video, media controller) accordingly.
+ *
+ * A sequencer exposes usual media element controls such as "play", "pause"
+ * methods as well as "currentTime" and "playbackRate" attributes. Internally,
+ * calling these methods or setting these attributes update the timing object's
+ * state vector, which should in turn affect the HTML media elements that the
+ * sequencer harnesses.
+ *
+ * Said differently, commands sent to a sequencer are not directly applied to
+ * the HTML media elements under control. Everything goes through the timing
+ * object to enable cross-device synchronization effects.
+ *
+ * TODO: add logic to handle buffering hiccups in media elements.
+ * TODO: add logic to remove elements from the list of controlled elements.
+ */
+
+// Ensure "define" is defined in node.js in the absence of require.js
+// See: https://github.com/jrburke/amdefine
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
+
+define(function (require) {
+  var woodman = require('woodman');
+  var logger = woodman.getLogger('Sequencer');
+
+  var EventTarget = require('event-target');
+  var TimingObject = require('./TimingObject');
+  var StateVector = require('./StateVector');
+
+
+  /**
+   * Constructor of a sequencer
+   *
+   * @class
+   * @param {TimingObject} timing The timing object attached to the sequencer
+   * @param {Object} options Sequencer settings
+   */
+  var Sequencer = function (timing, options) {
+    var self = this;
+    options = options || {};    
+
+    if (!timing || (!timing instanceof TimingObject)) {
+      throw new Error('No timing object provided');
+    }
+
+    /**
+     * The sequencer's internal settings
+     */
+    var settings = {
+      // Media elements are considered in sync with the timing object if the
+      // difference between the position they report and the position of the
+      // timing object is below that threshold (in seconds).
+      minDiff: options.minDiff || 0.010,
+
+      // Maximum delay for catching up (in seconds).
+      // If the code cannot meet the maxDelay constraint,
+      // it will have the media element directly seek to the right position.
+      maxDelay: options.maxDelay || 1.0,
+
+      // Amortization period (in seconds).
+      // The amortization period is used when adjustments are made to
+      // the playback rate of the video.
+      amortPeriod: options.amortPeriod || 2.0
+    };
+
+
+    /**
+     * The list of Media elements controlled by this sequencer.
+     *
+     * For each media element, the sequencer maintains a state vector
+     * representation of the element's position and velocity, a drift rate
+     * to adjust the playback rate, whether we asked the media element to
+     * seek or not, and whether there is an amortization period running for
+     * the element
+     *
+     * {
+     *   vector: {},
+     *   driftRate: 0.0,
+     *   seeked: false,
+     *   amortization: false,
+     *   element: {}
+     * }
+     */
+    var controlledElements = [];
+
+
+    /**
+     * The timing object's state vector last time we checked it.
+     * This variable is used in particular at the end of the amortization
+     * period to compute the media element's drift rate
+     */
+    var timingVector = null;
+
+
+    /**
+     * Pointer to the amortization period timeout.
+     * The sequencer uses only one amortization period for all media elements
+     * under control.
+     */
+    var amortTimeout = null;
+
+
+    Object.defineProperties(this, {
+      /**
+       * Report the state of the underlying timing object
+       *
+       * TODO: should that also take into account the state of the controlled
+       * elements? Hard to find a proper definition though
+       */
+      readyState: {
+        get: function () {
+          return timingProvider.readyState;
+        }
+      },
+
+
+      /**
+       * The currentTime attribute returns the position that all controlled
+       * media elements should be at, in other words the position of the
+       * sequencer when this method is called.
+       *
+       * On setting, the timing object's state vector is updated with the
+       * provided value, which will (asynchronously) affect all controlled
+       * media elements.
+       *
+       * Note that getting "currentTime" right after setting it may not return
+       * the value that was just set.
+       */
+      currentTime: {
+        get: function () {
+          return timing.query().computePosition(Date.now() / 1000.0);
+        },
+        set: function (value) {
+          timing.update(value, null);
+        }
+      },
+
+
+      /**
+       * The current playback rate of the sequencer (controlled media elements
+       * may have a slightly different playback rate since the role of the
+       * sequencer is precisely to adjust their playback rate to ensure they
+       * keep up with the sequencer's position.
+       *
+       * On setting, the timing object's state vector is updated with the
+       * provided value, which will (asynchronously) affect all controlled
+       * media elements.
+       *
+       * Note that getting "playbackRate" right after setting it may not return
+       * the value that was just set.
+       */
+      playbackRate: {
+        get: function () {
+          return timing.query().computeVelocity(Date.now() / 1000.0);
+        },
+        set: function (value) {
+          timing.update(null, value);
+        }
+      }
+    });
+
+
+    /**
+     * Start playing the controlled elements
+     *
+     * @function
+     */
+    this.play = function () {
+      timing.update(null, 1.0);
+    };
+
+
+    /**
+     * Pause playback
+     *
+     * @function
+     */
+    this.pause = function () {
+      timing.update(null, 0.0);
+    };
+
+
+    /**
+     * Add a media element to the list of elements controlled by this
+     * sequencer
+     *
+     * @function
+     * @param {MediaElement} element The media element to associate with the
+     *  sequencer.
+     */
+    this.addMediaElement = function (element) {
+      var found = false;
+      controlledElements.forEach(function (wrappedEl) {
+        if (wrappedEl.element === element) {
+          found = true;
+        }
+      });
+      if (found) {
+        return;
+      }
+      controlledElements.push({
+        element: element,
+        vector: null,
+        driftRate: 0.0,
+        seeked: false,
+        amortization: false
+      });
+    };
+
+
+    /**
+     * Helper function that cancels a running amortization period
+     */
+    var cancelAmortizationPeriod = function () {
+      if (!amortTimeout) {
+        return;
+      }
+      clearTimeout(amortTimeout);
+      amortTimeout = null;
+      controlledElements.forEach(function (wrappedEl) {
+        wrappedEl.amortization = false;
+        wrappedEl.seeked = false;
+      });
+    };
+
+
+    /**
+     * Helper function to stop the playback adjustment once the amortization
+     * period is over.
+     */
+    var stopAmortizationPeriod = function () {
+      var now = Date.now() / 1000.0;
+      amortTimeout = null;
+
+      controlledElements.forEach(function (wrappedEl) {
+        // Don't adjust playback rate and drift rate if video was seeked.
+        // The seek operation often introduces artificial delays.
+        if (wrappedEl.seeked) {
+          logger.log('end of amortization period for seek');
+          wrappedEl.seeked = false;
+          return;
+        }
+
+        // Compute the difference between the position the video should be and
+        // the position it is reported to be at.
+        var diff = wrappedEl.vector.computePosition(now) -
+          wrappedEl.element.currentTime;
+
+        // Compute the new video drift rate
+        wrappedEl.driftRate = diff / (now - wrappedEl.vector.timestamp);
+
+        // Switch back to the current vector's velocity,
+        // adjusted with the newly computed drift rate
+        wrappedEl.vector.velocity = timingVector.velocity + wrappedEl.driftRate;
+        wrappedEl.element.playbackRate = wrappedEl.vector.velocity;
+
+        logger.log('end of amortization period',
+          'new drift=' + wrappedEl.driftRate,
+          'playback rate=' + wrappedEl.vector.velocity);
+      });
+    };
+
+
+
+    /**
+     * React to timing object's changes, harnessing the controlled
+     * elements to align them with the timing object's position and velocity
+     */
+    var onTimingChange = function () {
+      cancelAmortizationPeriod();
+      controlElements();
+    };
+
+
+    /**
+     * Ensure media elements are aligned with the current timing object's
+     * state vector
+     */
+    var controlElements = function () {
+      // Do not adjust anything during an amortization period
+      if (amortTimeout) {
+        return;
+      }
+
+      // Get new readings from Timing object
+      timingVector = timing.query();
+
+      controlledElements.forEach(controlElement);
+
+      var amortNeeded = false;
+      controlledElements.forEach(function (wrappedEl) {
+        if (wrappedEl.amortization) {
+          amortNeeded = true;
+        }
+      });
+
+      if (amortNeeded) {
+        logger.info('start amortization period');
+        amortTimeout = setTimeout(stopAmortizationPeriod, settings.amortPeriod * 1000);
+      }
+
+      // Queue a task to fire a simple event named "timeupdate"
+      setTimeout(function () {
+        self.dispatchEvent({
+          type: 'timeupdate'
+        }, 0);
+      });
+    };
+
+
+    /**
+     * Ensure the given media element (wrapped in info structure) is aligned
+     * with the current timing object's state vector
+     */
+    var controlElement = function (wrappedEl) {
+      var element = wrappedEl.element;
+      var diff = 0.0;
+      var futurePos = 0.0;
+
+      if ((timingVector.velocity === 0.0) &&
+          (timingVector.acceleration === 0.0)) {
+        logger.info('stop element and seek to right position');
+        element.pause();
+        element.currentTime = timingVector.position;
+        wrappedEl.vector = new StateVector(timingVector);
+      }
+      else if (element.paused) {
+        logger.info('play video');
+        wrappedEl.vector = new StateVector({
+          position: timingVector.position,
+          velocity: timingVector.velocity + wrappedEl.driftRate,
+          acceleration: 0.0,
+          timestamp: timingVector.timestamp
+        });
+        wrappedEl.seeked = true;
+        wrappedEl.amortization = true;
+        element.currentTime = wrappedEl.vector.position;
+        element.playbackRate = wrappedEl.vector.velocity;
+        element.play();
+      }
+      else {
+        wrappedEl.vector = new StateVector({
+          position: element.currentTime,
+          velocity: wrappedEl.vector.velocity,
+        });
+        diff = timingVector.position - wrappedEl.vector.position;
+        if (Math.abs(diff) < settings.minDiff) {
+          logger.info('video and vector are in sync!');
+        }
+        else if (Math.abs(diff) > settings.maxDelay) {
+          logger.info('seek video to pos={}', timingVector.position);
+          wrappedEl.vector.position = timingVector.position;
+          wrappedEl.vector.velocity = timingVector.velocity + wrappedEl.driftRate;
+          wrappedEl.seeked = true;
+          wrappedEl.amortization = true;
+          element.currentTime = wrappedEl.vector.position;
+          element.playbackRate = wrappedEl.vector.velocity;
+        }
+        else {
+          futurePos = timingVector.computePosition(
+            timingVector.timestamp + settings.amortPeriod);
+          wrappedEl.vector.velocity =
+            wrappedEl.driftRate +
+            (futurePos - wrappedEl.vector.position) / settings.amortPeriod;
+          wrappedEl.amortization = true;
+          element.playbackRate = wrappedEl.vector.velocity;
+          logger.info('new playbackrate={}', wrappedEl.vector.velocity);
+        }
+      }
+    };
+    
+
+    /**********************************************************************
+    Listen to the timing object
+    **********************************************************************/
+    logger.info('add listener to "timeupdate" events...');
+    timing.addEventListener('timeupdate', controlElements);
+    timing.addEventListener('change', onTimingChange);
+    logger.info('add listener to "timeupdate" events... done');
+
+    logger.info('add listener to "readystatechange" events...');
+    timing.addEventListener('readystatechange', function (evt) {
+      self.dispatchEvent(evt);
+    });
+    logger.info('add listener to "readystatechange" events... done');
+
+    logger.info('created');
+  };
+
+
+  // Sequencer implements EventTarget
+  Sequencer.prototype.addEventListener = EventTarget.addEventListener;
+  Sequencer.prototype.removeEventListener = EventTarget.removeEventListener;
+  Sequencer.prototype.dispatchEvent = EventTarget.dispatchEvent;
+
+
+  // Expose the class to the outer world
+  return Sequencer;
+});
